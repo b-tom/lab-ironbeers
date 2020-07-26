b12385f9b302bfaaee406c56f1f5b69f1e073c20
Tomas Bennasar <tomas.bennasar@gmail.com>
2020-07-25 23:44:30 -0400

    iterations completed

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // Register the location for handlebars partials here:
 
@@ -18,8 +20,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req,res) => {
+  // const beers = await punkAPI
+  // .getBeers({});
+  // console.log(beers);
+// });
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    console.log('Beers from API: ', beers)
+    res.render('beers', {beers})})
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req,res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+    res.render('randomBeers', {responseFromAPI})
+  })
+  .catch(error => console.log(error))
+})
+
+//index.js link not working. Had to do the above.
+// app.get('/beers', require('./routes/index'));
+// app.use('/random-beers', require('./routes/index'))
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const router = express();
+
+// router.get('/beers', (req, res, next) => {
+//     res.render('beers');
+// })
+
+// router.get('/random-beers', (req, res, next) => {
+//     res.render('randomBeers')
+// })
+
+// module.exports = router;
