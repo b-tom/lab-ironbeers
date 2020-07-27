f3bd0906b7e27e0a5f68c9398be299fdd40c2b7b
Tomas Bennasar <tomas.bennasar@gmail.com>
2020-07-26 20:32:56 -0400

    completed last iterations

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
-
-
-// Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
 
 // Add the route handlers here:
 
